3731792bfd9de9e9b44de0bc63a360102b03a0ab
dongapple <hg971027@gmail.com>
2023-08-09 15:18:26 +0900

    컴포넌트 분리/관리하기

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import RootLayout from "./layout/RootLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <RootLayout>페이지의 주요 컨텐츠</RootLayout>
+    </div>
+  );
+}
+
+export default App;
